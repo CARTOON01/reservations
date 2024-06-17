25b7e9db50f4ec34764b5c7fd7c8c7581b55fc19
CARTOON <brianngugi224@gmail.com>
2024-06-17 16:21:16 +0300

    Add RegisterModal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const inter = Nunito({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
-          <Modal actionLabel="Submit" title="Login Modal" isOpen/>
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
