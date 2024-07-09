2819c9a14b4c13521b3746d2fdd52e0ba8be6a5e
CARTOON <brianngugi224@gmail.com>
2024-07-09 20:39:44 +0300

    Social login

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -43,7 +43,7 @@ const RegisterModal = () => {
                 outline
                 label="Continue with Google"
                 icon={FcGoogle}
-                onClick={() => {}}
+                onClick={() => signIn('google')}
             />
             <Button
                 outline
