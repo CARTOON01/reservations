f53acfd8fe9b65611f5443026c695dd35ded1228
CARTOON <brianngugi224@gmail.com>
2024-07-11 22:09:35 +0300

    add location modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
     CATEGORY = 0,
@@ -110,11 +111,25 @@ const RentModal = () => {
         </div>
     )
 
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="fle flex-col gap-8">
+                <Heading
+                    title="Where is your place Located?"
+                    subtitle="Help guests find you"
+                />
+                <CountrySelect
+
+                />
+            </div>
+        )
+    }
+
     return ( 
         <Modal 
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
