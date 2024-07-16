dc636944d4d30a9959960f96961572a479ea2b3b
CARTOON <brianngugi224@gmail.com>
2024-07-16 21:46:24 +0300

    Add Leaflet library for interactive maps and integrate with RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -48,6 +49,10 @@ const RentModal = () => {
     });
 
     const category = watch('category');
+    const location = watch('location');
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -119,7 +124,11 @@ const RentModal = () => {
                     subtitle="Help guests find you"
                 />
                 <CountrySelect
-
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map
+                    center={location?.latlng}
                 />
             </div>
         )
